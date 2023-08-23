47331363961a161c9d71376029bfb8c7a98f1ec6
Rafael de Luca <deluca1712@gmail.com>
2023-08-22 21:52:32 -0300

    Test find product when product name is inform

---
+++ src/test/java/com/rafaeldeluca/dscommerce/controllers/it/ProductControllerIT.java
@@ -0,0 +1,49 @@
+package com.rafaeldeluca.dscommerce.controllers.it;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// annotations para carregar o contexto das aplicacoes
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String productName;
+
+    @BeforeEach
+    public void setUp () {
+
+        productName = "Smart TV";
+
+    }
+
+    // product mock data
+    // {"id":2,"name":"Smart TV","price":2190.0,"imgUrl":"https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"}
+    @Test
+    public void findAllProductsShouldReturnPageWhenProductNameIsNotEmpty  () throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(get("/products?name={productName}",productName)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.content[0].id").value(2L));
+        resultActions.andExpect(jsonPath("$.content[0].name").value("Smart TV"));
+        resultActions.andExpect(jsonPath("$.content[0].price").value(2190.0));
+        resultActions.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"));
+    }
+}
